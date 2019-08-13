9cc9e0c39895648d07cf69fc336acbce03dd1f18
Chris McCluskey <cmccluskey@gmail.com>
2019-08-13 12:36:02 -0700

    Complete token contract with tests

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -10,25 +10,44 @@ contract Token {
 	string public symbol = "MUSK";
 	uint256 public decimals = 18; // unit256 means unsigned intiger and 256 is number of bites. A number without a sign 1 is a uint, -1 has a sign (-). We always want these numbers to be positive, can't have negaitve balances.
 	uint256 public totalSupply;
-
-	// Track balances - Stores information
-	mapping(address => uint256) public balanceOf; //mapping is like a dictionary in python, key - value pairs (address: token_balance)
+	mapping(address => uint256) public balanceOf; // Track balances - Stores information. Mapping is like a dictionary in python, key - value pairs (address: token_balance)
+	mapping(address => mapping(address => uint256)) public allowance;
 	
 	// Events
 	event Transfer(address indexed from, address indexed to, uint256 value);
-
+	event Approval(address indexed owner, address indexed spender, uint256 value);
 	constructor() public {
 		totalSupply = 1000000 * (10 ** decimals);
 		balanceOf[msg.sender] = totalSupply;
 	}
     // Send tokens - Impliments behavior / fuctionality
 	function transfer(address _to, uint256 _value) public returns (bool success) {
-		require(_to != address(0));
 		require(balanceOf[msg.sender] >= _value);
-		balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+		_transfer(msg.sender, _to, _value);
+		return true;
+	}
+
+	function _transfer(address _from, address _to, uint256 _value) internal {
+		require(_to != address(0));
+		balanceOf[_from] = balanceOf[_from].sub(_value);
 		balanceOf[_to] = balanceOf[_to].add(_value);
-		emit Transfer(msg.sender, _to, _value);
-		return true; 
+		emit Transfer(_from, _to, _value);
+	}
+
+	// Approve tokens 
+	function approve(address _spender, uint256 _value) public returns (bool success) {
+		require(_spender != address(0));
+		allowance[msg.sender][_spender] = _value;
+		emit Approval(msg.sender, _spender, _value);
+		return true;
+	}
+
+	function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+		require(_value <= balanceOf[_from]);
+		require(_value <= allowance[_from][msg.sender]);
+		allowance[_from][msg.sender] = allowance[_from][msg.sender].sub(_value);
+		_transfer(_from, _to, _value);
+		return true;
 	}
 }
 
