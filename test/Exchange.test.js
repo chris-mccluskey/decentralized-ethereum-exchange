ae4fa0cff7c6711f6d7db2e0a1ba768d38e0c9cc
Chris McCluskey <cmccluskey@gmail.com>
2019-10-04 15:40:59 +0700

    Small updated progress

--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -1,16 +1,19 @@
 import { tokens, EVM_REVERT } from '../helpers'
 
+const Token = artifacts.require('./Token')
 const Exchange = artifacts.require('./Exchange')
 
 require('chai')
   .use(require('chai-as-promised'))
   .should()
 
-contract('Exchange', ([deployer, feeAccount]) => {
+contract('Exchange', ([deployer, feeAccount, user1]) => {
+  let token
   let exchange
   const feePercent = 10
 
   beforeEach(async () => {
+    token = await Token.new()
     exchange = await Exchange.new(feeAccount, feePercent)
   })
 
@@ -29,13 +32,20 @@ contract('Exchange', ([deployer, feeAccount]) => {
 })
 
 describe('depositing tokens', () => {
+  let result
+
+  beforeEach(async () => {
+    await token.approve(exchange.address, tokens(10), { from: user1 })
+    const result await exchange.depositToken(token.address, tokens(10), { from: user1})
+  })
+
   describe('success', () => {
     it('tracks the token deposit', async () => {
 
     })
   })
   describe('failure', () => {
-    
+
   })
   })
 })
