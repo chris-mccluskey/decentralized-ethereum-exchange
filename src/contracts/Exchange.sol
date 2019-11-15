1f6c2bfccbac8808249c167bd697051d3eb5c321
Chris McCluskey <cmccluskey@gmail.com>
2019-11-15 12:21:28 +0700

    commends added

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -1,17 +1,17 @@
-/* pragma solidity ^0.5.0;
+pragma solidity ^0.5.0;
 
 contract Exchange {
     // Variables
     address public feeAccount; // the account that receives exchange fees
     uint256 public feePercent; // the fee percentage
-    constructor (address _feeAccount, uint256 _feePercent) public {
+    constructor (address _feeAccount, uint256 _feePercent) public { // _ is used for local variables when they represent the values that will be passed to a state variable. _ is convention
         feeAccount = _feeAccount;
         feePercent = _feePercent;
-    /* }
+    }
     function depositToken(address _token, uint _amount) public {
       Token(_token).transferFrom(msg.sender, address(this), _amount);
-    } */
-// } 
+    }
+}
 
 // TODO:
 // [ ] Set the fee account
