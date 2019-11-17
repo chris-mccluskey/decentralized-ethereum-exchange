11102320b9cf919df61f6bf7c25c70833f196087
Chris McCluskey <cmccluskey@gmail.com>
2019-11-17 18:12:19 +0700

    updates

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -5,7 +5,7 @@ contract Exchange {
     // Variables
     address public feeAccount; // the account that receives exchange fees
     uint256 public feePercent; // the fee percentage we take from exchange fees
-	  mapping(address => mapping(address => uint256)) public tokens; // A nested mapping. First key will be the token address, 2nd key will be the address of the user that has deposited tokens themselves, value the amount of tokens in the users address.
+	  mapping(address => mapping(address => uint256)) public tokens; // A nested mapping. First key will be the token address, 2nd key will be the address of the user that has deposited tokens themselves, value the amount of tokens in the users address
     constructor (address _feeAccount, uint256 _feePercent) public { // _ is used for local variables when they represent the values that will be passed to a state variable. _ is convention
         feeAccount = _feeAccount;
         feePercent = _feePercent;
