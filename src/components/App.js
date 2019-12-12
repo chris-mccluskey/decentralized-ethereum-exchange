87ff1406a77d3fc1a9d3def82144feb93a33c5f0
Chris McCluskey <cmccluskey@gmail.com>
2019-12-12 23:23:45 +0700

    Begining react front end complete, web3 connection started

---
+++ src/components/App.js
@@ -0,0 +1,107 @@
+import React, { Component } from 'react';
+import './App.css';
+import Web3 from 'web3';
+// If adding bootstrap classes we must rename class to className to avoid a claching with React
+class App extends Component {
+  componentWillMount() {
+    this.loadBlockchainData()
+  }
+
+
+  async loadBlockchainData() {
+    const web3 = new Web3(Web3.givenProvider || 'http://localhost:7545')
+    console.log("web3", web3)
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-info">
+          <a className="navbar-brand" href="/#">Navbar</a>
+          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav">
+              <li className="nav-item">
+                <a className="nav-link" href="/#">Link 1</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/#">Link 2</a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="/#">Link 3</a>
+              </li>
+            </ul>
+          </div>
+        </nav>
+        <div className="content">
+          <div className="vertical-split">
+            <div className="card bg-dark text-white">
+              <div className="card-header">
+                Card Title
+              </div>
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="/#" className="card-link">Card link</a>
+              </div>
+            </div>
+            <div className="card bg-dark text-white">
+              <div className="card-header">
+                Card Title
+              </div>
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="/#" className="card-link">Card link</a>
+              </div>
+            </div>
+          </div>
+          <div className="vertical">
+            <div className="card bg-dark text-white">
+              <div className="card-header">
+                Card Title
+              </div>
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="/#" className="card-link">Card link</a>
+              </div>
+            </div>
+          </div>
+          <div className="vertical-split">
+            <div className="card bg-dark text-white">
+              <div className="card-header">
+                Card Title
+              </div>
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="/#" className="card-link">Card link</a>
+              </div>
+            </div>
+            <div className="card bg-dark text-white">
+              <div className="card-header">
+                Card Title
+              </div>
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="/#" className="card-link">Card link</a>
+              </div>
+            </div>
+          </div>
+          <div className="vertical">
+            <div className="card bg-dark text-white">
+              <div className="card-header">
+                Card Title
+              </div>
+              <div className="card-body">
+                <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <a href="/#" className="card-link">Card link</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
